513f137c552786f4c673e986e3446c947f5d445c
davidbetteridge <david.betteridge@zeromission.io>
2025-11-12 20:59:18 +0000

    3 for 2

--- c_sharp/basket/Basket.cs
+++ c_sharp/basket/Basket.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Runtime.CompilerServices;
 using System.Text;
 
 public class Basket
@@ -6,7 +7,8 @@ public class Basket
 
     private readonly Dictionary<string, double> stockItems = new Dictionary<string, double>(StringComparer.CurrentCultureIgnoreCase)
     {
-        {"milk", 1.5}
+        {"milk", 1.5},
+        {"bread", 1.0}
     };
 
     private List<string> items = new List<string>();
@@ -16,14 +18,32 @@ public class Basket
         if (items.Count == 0)
             return "No items";
 
+
+        var groupedItems = items
+            .Select((s, i) => new { Order = i, Item = s })
+            .GroupBy(x => x.Item)
+            .Select(x => new { ItemName = x.Key, Quantity = x.Count(), Order = x.Min(y => y.Order) })
+            .OrderBy(o => o.Order)
+            .ToList();
+
         var total = 0.0;
         var itemCounter = 1;
         StringBuilder sb = new StringBuilder();
-        foreach (var item in items)
+
+        foreach (var item in groupedItems)
         {
-            var cost = stockItems[item];
-            total+=cost;
-            sb.AppendLine($"{itemCounter}) {item}. 1 @ {cost:C} = {cost:C}");
+            var cost = stockItems[item.ItemName];
+            var lineCost = item.Quantity * cost;
+            total += lineCost;
+            sb.AppendLine($"{itemCounter++}) {item.ItemName}. {item.Quantity} @ {cost:C} = {lineCost:C}");
+
+            var discountedItems = (int)(item.Quantity / 3);
+            if (discountedItems > 0)
+            {
+                var discount = discountedItems * cost;
+                sb.AppendLine($"Discount {discount:C} 3For2");
+                total -= discount;
+            }
         }
         sb.Append($"Total: {total:C}");
         return sb.ToString();
